44a4c159ad138e9eda2a8212ce31e3a2dffc37e1
Andrea <andreakwilliams1@gmail.com>
2019-10-29 22:38:58 -0400

    created script to talk to database

---
+++ bamazonCustomer.js
@@ -0,0 +1,59 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+
+    user: 'root',
+    password: 'DIVAGIRL1020!',
+    database: 'bamazon'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connection successful');
+    displayItems();
+});
+
+var displayItems = function () {
+    connection.query(`SELECT * FROM products`, (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        //console.log(res);
+        connection.end();
+        prompt(res)
+
+    }
+    );
+}
+//code to prompt items to be chosen
+function prompt(res) {
+    console.log(res)
+    const names = res.map((product, index) => {
+        return product.product_name
+    })
+    inquirer.prompt([
+        {
+            name: 'product_name',
+            type: 'list',
+            message: 'What would you like to purchase?',
+            choices: names
+        },
+        {
+            name: 'units',
+            type: 'input',
+            message: 'How many units would you like to purchase?',
+
+        }
+    ]).then(answers => {
+        connection.query(`SELECT stock_quantity FROM products WHERE product_name = ${answers.product_name}`, (err, res) => {
+            console.log(res)
+            console.log('HERE')
+        })
+        console.log(answers)
+    })
+}
